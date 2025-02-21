b2e4f23af1dc6516260fef0c21e1a87f2f91d173
GaganTantwar <panthigagan478@gmail.com>
2025-02-21 20:41:48 +0530

    Gagan Tantwar Added :Helloworld Spring App to display Hello from BridgeLabz” in a Web Page

---
+++ SpringLearning/src/main/java/com/springlearning/demo/controller/uc2/SpringApplicationMVC.java
@@ -0,0 +1,11 @@
+package com.springlearning.demo.controller.uc2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationMVC {
+    public static void main(String args[]){
+        SpringApplication.run(SpringApplicationMVC.class,args);
+    }
+
+}
