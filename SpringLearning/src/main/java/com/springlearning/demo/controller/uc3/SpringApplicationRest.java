135505bcd2aa1d0836e0b5659b0da895b2d30035
GaganTantwar <panthigagan478@gmail.com>
2025-02-21 20:45:22 +0530

    Gagan Tantwar Added : a Helloworld Spring App to display “Hello from BridgeLabz” in REST Call

---
+++ SpringLearning/src/main/java/com/springlearning/demo/controller/uc3/SpringApplicationRest.java
@@ -0,0 +1,10 @@
+package com.springlearning.demo.controller.uc3;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationRest {
+    public static void main(String args[]){
+        SpringApplication.run(SpringApplicationRest.class,args);
+    }
+}
