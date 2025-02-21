8a08e82f3a4d4399245eb3bff2aadc6f606345aa
GaganTantwar <panthigagan478@gmail.com>
2025-02-21 20:56:21 +0530

    Gagan Tantwar Added: Spring Learning

---
+++ SpringLearning/src/main/java/com/springlearning/demo/Uc2/HelloWebController.java
@@ -0,0 +1,18 @@
+package com.springlearning.demo.controller.uc2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+    @GetMapping("/web")
+    public String sayHello(){
+        return "Hello From BridgeLabz";
+    }
+    @GetMapping("/web/message")
+    public String message(Model model){
+        model.addAttribute("message" +
+                "This is a custom message");
+        return "message";
+    }
+}
